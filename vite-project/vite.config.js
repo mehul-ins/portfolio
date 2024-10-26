40256e5f02bcbceb0179df64731ff72c826b39aa
Mehul  Vig <mehulvig6@gmail.com>
2024-10-26 17:20:48 +0530

    .

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Adjust if deploying in a subdirectory
-  build: {
-    outDir: 'dist' // The default output directory for Vercel
-  }
-});
+  base: '/subdirectory-path/', // Replace with your subdirectory path if needed
+})
