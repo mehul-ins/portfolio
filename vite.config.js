9cf07a0cc991166e2b8f864dd4a61d33ef90c28c
Raghav Yadav <isitraghav@gmail.com>
2024-10-26 17:41:26 +0530

    ready for deployment on vercel/netlify

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/subdirectory-path/', // Replace with your subdirectory path if needed
+  base: '/', // Replace with your subdirectory path if needed
 })
