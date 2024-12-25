bd726cbece80ab6dd53fa04939f9484f6f8e3959
mxhul_10 <mehul.vig6@gmail.com>
2024-12-25 19:44:02 +0530

    scroll element added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -19,18 +19,18 @@ function Card({ title, image, event, role }) {
 export default function About() {
   return (
     <>
-      <div className='h-100' id='about-me'>
+      <div className='h-100 px-4' id='about-me'>
         <p className='text-3xl p-7'>Experience</p>
-        <div className='flex justify-center items-center flex-col sm:flex-row'>
+        <div className='flex justify-center items-center  flex-col sm:flex-row flex-shrink-0 overflow-x-scroll scroll-smooth snap-x snap-mandatory snap-start'>
           <Card title="Volunteer"
             image={sih}
             event="Smart India Hackathon"
-            role="Marketing executive" />
+            role="Marketing Executive" />
 
           <Card title="Experience"
             image={acm}
             event="ACM BMU"
-            role="Executive Committee" />
+            role="EC member" />
 
           <Card title="Volunteer"
             image={savera}
@@ -40,7 +40,7 @@ export default function About() {
       </div>
       <div className='h-0.5 bg-[#3d444d]'></div>
 
-
+{/* Tech stack */}
       <p className='text-3xl p-7'>Tech Stack</p>
       <div className='flex justify-center'>
         <div className='h-24 rounded-xl bg-[#3d444d] w-24 m-5 flex justify-center items-center hover:cursor-pointer hover:bg-transparent hover:border-2 hover:shadow-lg hover:shadow-[rgb(25,104,31)]'>
@@ -55,6 +55,9 @@ export default function About() {
           <i class='bx bxl-mongodb text-7xl text-green-400' ></i>
         </div>
 
+        <div className='h-24 rounded-xl bg-[#3d444d] w-24 m-5 flex justify-center items-center hover:cursor-pointer hover:bg-transparent hover:border-2 hover:shadow-lg hover:shadow-[rgb(25,104,31)]' >
+          <i class='bx bxl-nodejs text-7xl text-green-800' ></i>
+        </div>
         <div className='h-24 rounded-xl bg-[#3d444d] w-24 m-5 flex justify-center items-center hover:cursor-pointer hover:bg-transparent hover:border-2 hover:shadow-lg hover:shadow-[rgb(25,104,31)]' >
           <i class='bx bxl-nodejs text-7xl text-green-800' ></i>
         </div>
